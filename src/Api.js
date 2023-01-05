127947002da8a4f3a1e12ce275b68ae0371f9561
Kejni Koçi <kejnikoci2000@gmail.com>
2023-01-05 21:47:30 +0100

    firstCommit

---
+++ src/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "865b5b4cfamsh5bd880f2a44170cp145405jsna245f75a3c1a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
