c74c232cf5893c32b9ef7fc81fa5c63c338a0dca
Kejni Koçi <59995634+kocikejni@users.noreply.github.com>
2023-01-11 21:42:37 +0100

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "865b5b4cfamsh5bd880f2a44170cp145405jsna245f75a3c1a",
+    "X-RapidAPI-Key": "X-RapidAPI-Key",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "5d52494e210c1b97a265e1c48a1046fa";
+export const WEATHER_API_KEY = "WEATHER_API_KEY";
