7894f0fe0ab204df42ab810ddf051fc3b39902b6
Kejni Koçi <kejnikoci2000@gmail.com>
2023-01-11 21:41:01 +0100

    final

--- src/components/search/currentWeather/CurrentWeather.js
+++ src/components/search/currentWeather/CurrentWeather.js
@@ -1,33 +1,33 @@
 import React from "react";
 import "./CurrentWeather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Tirana</p>
-          <p className="weatherDescription">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weatherDescription">{data.weather[0].main}</p>
         </div>
-        <img alt="weather" className="weatherIcon" src="icons/01d.png" />
+        <img alt="weather" className="weatherIcon" src={`icons/${data.weather[0].icon}.png`} />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameterRow">
             <span className="parameterLabel">Details</span>
           </div>
           <div className="parameterRow">
             <span className="parameterLabel">Feels like</span>
-            <span className="parameterValue">22°C</span>
+            <span className="parameterValue">{Math.round(data.main.feels_like)}°C</span>
           </div>
           <div className="parameterRow">
             <span className="parameterLabel">Wind</span>
-            <span className="parameterValue">22km/h</span>
+            <span className="parameterValue">{data.wind.speed}km/h</span>
           </div>
           <div className="parameterRow">
             <span className="parameterLabel">Humidity</span>
-            <span className="parameterValue">22%</span>
+            <span className="parameterValue">{data.main.humidity}</span>
           </div>
         </div>
       </div>
